ff9cf445057d4f410b57dac14c98b4052e5386f1
Alex Egana <48147071+usodst@users.noreply.github.com>
2023-09-28 11:28:07 -0700

    Moves autocmd to init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"terraform", "markdown", "lua", "tf"},
+  callback = function ()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end
+})
