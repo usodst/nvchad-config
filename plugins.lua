fc653346f3b9d0d29dd182a2a9d4027b522754f8
Alex Egana <48147071+usodst@users.noreply.github.com>
2023-10-13 12:33:54 -0700

    Adds terraform to treesitter config

--- plugins.lua
+++ plugins.lua
@@ -1,6 +1,14 @@
 -- In order to modify the `lspconfig` configuration:
 local plugins = {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "terraform"
+      }
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
