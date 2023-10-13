31ba7ed3316aa31a202b5d42ff7949e14908173d
Alex Egana <48147071+usodst@users.noreply.github.com>
2023-10-13 12:30:49 -0700

    Two spaces for shell scripts

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"terraform", "markdown", "lua", "tf"},
+  pattern = {"terraform", "markdown", "lua", "tf", "sh"},
   callback = function ()
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
