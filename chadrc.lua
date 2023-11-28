a75b58cd10a8ab9d6d87dd5e2ff7bbfbcaf98209
Alex Egana <48147071+usodst@users.noreply.github.com>
2023-11-27 18:08:38 -0800

    Enables transparency

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig 
 local M = {}
 
-M.ui = {theme = 'github_dark'}
+M.ui = {
+  theme        = 'github_dark',
+  transparency = true
+}
 M.plugins = 'custom.plugins'
 M.mappings = require("custom.configs.mappings")
 
