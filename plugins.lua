ed379add8230b0c73a5136a73335726e3890a456
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-01-23 21:06:20 -0800

    Adds model.nvim plugin

--- plugins.lua
+++ plugins.lua
@@ -65,6 +65,60 @@ local plugins = {
     config = function ()
       require("dapui").setup()
     end
+  },
+  {
+    'gsuuon/model.nvim',
+
+    -- Don't need these if lazy = false
+    cmd = { 'M', 'Model', 'Mchat' },
+    init = function()
+      vim.filetype.add({
+        extension = {
+          mchat = 'mchat',
+        }
+      })
+    end,
+    ft = 'mchat',
+
+    keys = {
+      {'<C-m>d', ':Mdelete<cr>', mode = 'n'},
+      {'<C-m>s', ':Mselect<cr>', mode = 'n'},
+      {'<C-m><space>', ':Mchat<cr>', mode = 'n' }
+    },
+
+    -- To override defaults add a config field and call setup()
+
+    config = function ()
+      local ollama = require('model.providers.ollama')
+      require('model').setup({
+        default_prompt = {
+            provider = ollama,
+            params = {
+              model = 'starling-lm'
+            },
+            builder = function(input)
+              return {
+                prompt = 'GPT4 Correct User: '
+                  .. input
+                  .. '<|end_of_turn|>GPT4 Correct Assistant: ',
+              }
+            end
+          }
+      })
+    end
+
+    -- config = function()
+    --   require('model').setup({
+    --     prompts = {..},
+    --     chats = {..},
+    --     ..
+    --   })
+    --
+    --   require('model.providers.llamacpp').setup({
+    --     binary = '~/path/to/server/binary',
+    --     models = '~/path/to/models/directory'
+    --   })
+    --end
   }
 }
 return plugins
