d68ca68afa4253e1192d425c3f4960047426242e
Alex Egana <48147071+usodst@users.noreply.github.com>
2024-03-19 11:27:09 -0700

    Uses vim.fs.find for selecting files

--- dap/adapters/coreclr.lua
+++ dap/adapters/coreclr.lua
@@ -1,11 +1,19 @@
 local M = {}
 
+local GetDllFiles = function()
+  local dllFiles = vim.fs.find(function(name, _)
+    return name:match ".*dll$"
+  end, { type = "file", limit = math.huge })
+  return dllFiles
+end
+
 M.adapter = {
-  type = 'executable',
-  command = vim.fs.find({"netcoredbg","netcoredbg.exe"},{
-    path = vim.fn.stdpath('data') .. "/mason/packages/netcoredbg"
+  type = "executable",
+  command = vim.fs.find({ "netcoredbg", "netcoredbg.exe" }, {
+    path = vim.fn.stdpath "data" .. "/mason/packages/netcoredbg",
+    type = "file",
   })[1],
-  args = {'--interpreter=vscode'}
+  args = { "--interpreter=vscode" },
 }
 
 M.config = {
@@ -14,7 +22,14 @@ M.config = {
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
-      return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+      -- return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+      local program = ""
+      vim.ui.select(GetDllFiles(), {
+        prompt = "Path to the dll:",
+      }, function(choice)
+        program = choice
+      end)
+      return program
     end,
   },
 }
